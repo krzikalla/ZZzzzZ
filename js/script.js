ec94d67a0675d873318b8fa5f24b031ae5ad7876
local <local>
2025-01-03 16:11:07 +0100

    Prevent screen sleep

--- js/script.js
+++ js/script.js
@@ -466,3 +466,29 @@ window.addEventListener('resize', function() {
 screen.orientation.addEventListener('change', function(event) {
   repositionFinder();
 });
+
+let wakeLock = null
+
+const acquireLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen')
+    console.log(`Lock acquired`)
+  } catch (err) {
+    console.log(`${err.name}, ${err.message}`)
+  }
+}
+
+const releaseLock = () => {
+  if (wakeLock) {
+    wakeLock.release().then(() => {
+      wakeLock = null
+    })
+  }
+}
+
+const handleVisibilityChange = () => (
+  document.visibilityState === 'hidden' ? releaseLock() : acquireLock()
+)
+
+document.addEventListener('visibilitychange', handleVisibilityChange)
+acquireLock();
